8b29da7a281cace5869cc2641f7551ffddda95bf
MustafaShams <77402878+Mustafa-Shams@users.noreply.github.com>
2024-11-26 21:18:44 +0500

    hero4.tsx

--- app/components/hero/hero4.tsx
+++ app/components/hero/hero4.tsx
@@ -6,7 +6,7 @@ import {
     CardDescription,
     CardFooter,
     CardHeader,
-    CardTitle,
+   
   } from "@/components/ui/card"
   import { Button } from "@/components/ui/button";
 export default function Hero4(){
